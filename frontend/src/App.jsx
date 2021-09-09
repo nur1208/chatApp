5b09a08e4463b221845e09a19684ae4e82d15307
nur <mode0o1208@yahoo.com>
2021-09-09 19:52:22 +0800

    building the chat component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import { ChatFeed } from "./components/ChatFeed";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Working!</h1>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="28b25b90-a10d-4e5a-8c23-74f7889769ff"
+      userName="nur"
+      userSecret="12341234"
+      renderChatFeed={(chatAppProps) => (
+        <ChatFeed {...chatAppProps} />
+      )}
+    />
   );
 }
 
